fb95cca4f0f0d160195851ab0e81a9211cf5ae44
olivecha <olivier.chabot.2@ens.etsmtl.ca>
2021-11-16 23:42:55 -0500

    added material for course 2

--- Exemples/exemple_conditional.py
+++ Exemples/exemple_conditional.py
@@ -21,3 +21,34 @@ elif number == 1001:
 # Si TOUTES Les conditions précédents ne sont pas vraies le code qui suit est exécuté
 else:
     print('Le nombre est impair')
+
+liste = ['a', 'b', 'c', 'd']
+nouvelle_liste = [0, 0, 0, 0]
+
+for i in range(4):
+    nouvelle_liste[i] = liste[i] + 'b'
+
+
+
+
+def salutation(phrase = 'Bonjour', nom=''):
+    """
+    Fonction qui salue quelqu'un
+    """
+    print(phrase + ' ' + nom + ' !')
+
+
+from random import randint
+help(randint)
+>>
+"""
+Help on method randint in module random:
+
+randint(a, b) method of random.Random instance
+    Return random integer in range [a, b], including both end points.
+"""
+
+
+
+
+
