8faff1a0423318b69bbc8d74b3495943fdb87d1c
olivecha <olivier.chabot.2@ens.etsmtl.ca>
2021-12-07 23:02:23 -0500

    added cours 5

---
+++ Cours5/cours5.py
@@ -0,0 +1,16 @@
+
+# Définir une variable avant la boucle
+my_count = 0
+# Pendant que ma variable respecte une condition
+while my_count < 10:
+    # Faire quelque chose
+    print(my_count)
+    # CHANGER LA VARIABLE (sinon boucle infinie)
+    my_count = my_count + 1
+
+
+import random
+
+random.randint()
+
+
